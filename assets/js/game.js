5f1eee8ab6cecdeac535e09f9be9e9bb4a99a810
Carlos vilares <car.vilares@gmail.com>
2023-05-10 13:56:14 +0000

    array of questions add

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,12 @@
-import { quotes } from './quotes.js';
+// Import the quotes array from the './quotes.js
+import { quotes } from './quotes.js'; 
 
+//array of questions for the quiz
 let questions = [
   {
     title: 'Who is the president of Sporting Clube de Portugal?',
-    alternatives: ['Frederico Varandas', 'Bruno de Carvalho', 'Pedro Proença', 'Jorge Jesus'],
-    correct: 0
+    alternatives: ['Frederico Varandas', 'Bruno de Carvalho', 'Pedro Proença', 'Jorge Jesus'],//array of possible answers
+    correct: 0 //index of the correct answer
   },
   {
     title: 'How many times has Sporting Clube de Portugal won the UEFA Champions League?',
@@ -33,88 +35,4 @@ let questions = [
   }
 ];
 
-let app = {
-  totalScore: 0,
-  wrongAnswers: 0,
-  currentPos: 0,
 
-  start: function () {
-    let alternatives = document.querySelectorAll('.alternative');
-    alternatives.forEach((element, index) => {
-      element.addEventListener('click', () => {
-        this.checkAnswer(index);
-      });
-    });
-    this.showQuestion(questions[this.currentPos]);
-    this.randomQuotes();
-  },
-
-  showQuestion: function (q) {
-    this.currentQuestion = q;
-    let titleDiv = document.getElementById('title');
-    titleDiv.textContent = q.title;
-    let alternatives = document.querySelectorAll('.alternative');
-    alternatives.forEach(function (element, index) {
-      element.textContent = q.alternatives[index];
-    });
-  },
-
-  nextQuestion: function () {
-    this.currentPos++;
-    if (this.currentPos === questions.length) {
-      this.currentPos = 0;
-    }
-  },
-
-  checkAnswer: function (user) {
-    if (this.currentQuestion.correct === user) {
-      console.log('Correct answer');
-      this.totalScore++;
-    } else {
-      console.log('Wrong answer');
-      this.wrongAnswers++;
-      if (this.wrongAnswers === 3) {
-        this.showDisappointment();
-        return; // Stop further execution
-      }
-    }
-    this.updateScore();
-    this.nextQuestion();
-    this.showQuestion(questions[this.currentPos]);
-    this.randomQuotes();
-
-    // Check if the total score reaches 5
-    if (this.totalScore === 5) {
-      this.showCongratulations();
-    }
-  },
-
-  showCongratulations: function () {
-    // Redirect to the congratulations page
-    window.location.href = 'congratulations.html';
-  },
-
-  showDisappointment: function () {
-    // Redirect to the disappointment page
-    window.location.href = 'end.html';
-  },
-  
-  
-  updateScore: function () {
-    let scoreDiv = document.getElementById('score');
-    scoreDiv.textContent = `Your score is: ${this.totalScore}`;
-  },
-
-  randomQuotes: async function () {
-    let randomSelection = Math.floor(Math.random() * quotes.length);
-    document.getElementById("quote").textContent = quotes[randomSelection].quote;
-    document.getElementById("author").textContent = quotes[randomSelection].character;
-  },
-  
-  resetScore: function () {
-    this.totalScore = 0;
-    this.updateScore();
-  }
-};
-
-app.start();
